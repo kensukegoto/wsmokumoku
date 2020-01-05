373fa8712a3007af80c174fdd78b4c3bbf5443f5
ws004 <n_ope@webstaff.jp>
2020-01-05 17:13:04 +0900

    JS実装前のデフォルト

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$('.mk-slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1 // 1枚ずつ
-});
+// $('.mk-slider').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1 // 1枚ずつ
+// });
