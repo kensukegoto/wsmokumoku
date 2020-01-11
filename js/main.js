0cb62d1f224bd0dc1877af42b10f1d8883663e08
kensuke goto <utogoken2@yahoo.co.jp>
2020-01-11 19:25:16 +0900

    カルーセル実装

--- js/main.js
+++ js/main.js
@@ -116,3 +116,66 @@
   });
 
 })();
+
+/**
+ * カルーセル
+ */
+(()=>{
+
+  var loopId = null;
+
+  function makeCarousel(){
+
+    var $carousel = $(".whatis__carousel");
+    var $set = $(".whatis__carousel__set");
+    var setW = $set.outerWidth();
+    var winW = $(window).innerWidth();
+    var num = Math.ceil(winW / setW);
+
+    $carousel.empty();
+    // num * 2 - 1 は１つのセットが最初からあるから
+    for(var i = 0;i < num * 2;i++){
+      $carousel.append($set.clone());
+    }
+
+    var left = 0;
+    var cW = num * setW;
+    var stop = false;
+    
+    loopId = setInterval(()=>{
+      if(stop) return;
+      if(cW + left === 1){
+        left = 0;
+      } else {
+        --left;
+      }
+      $carousel.css("transform","translateX(" + left + "px");
+    },33);
+
+    $carousel.on("mouseenter",()=>{
+      stop = true;
+    });
+    $carousel.on("mouseleave",()=>{
+      stop = false;
+    });
+  }
+
+  makeCarousel();
+
+  var resizeId = null;
+
+  $(window).on("resize",()=> {
+    
+    clearTimeout(resizeId);
+
+    resizeId = setTimeout(()=>{
+      clearTimeout(loopId)
+      makeCarousel();
+    },300)
+
+  });
+
+
+
+
+})();
