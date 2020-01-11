d8f8e5da38541ca29657e6de43a8198f31364ff2
kensuke goto <utogoken2@yahoo.co.jp>
2020-01-11 15:51:57 +0900

    スライダー実装

--- js/main.js
+++ js/main.js
@@ -29,3 +29,29 @@
   });
 
 })();
+
+(()=>{
+
+  $('.pickup__slider').slick({
+    appendArrows: $(".pickup"),
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1, // 1枚ずつ
+    responsive:[
+      {
+          breakpoint: 960,
+          settings:{
+              slidesToShow:2,
+          }
+      },
+      {
+          breakpoint: 600,
+          settings:{
+              slidesToShow:1,
+          }
+      },
+  ]
+  
+  });
+  
+})();
