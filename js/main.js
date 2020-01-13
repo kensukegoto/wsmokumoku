20ff1bc87aec8a280b6fcfc620358810166a95b9
kensuke goto <utogoken2@yahoo.co.jp>
2020-01-13 16:51:29 +0900

    トップへ戻る

--- js/main.js
+++ js/main.js
@@ -325,9 +325,26 @@
     }
   }
 
+})();
 
+/**
+ * トップへ戻る
+ */
+(()=>{
 
+  var $btn = $(".btnTop");
+  $btn.on("click",function(){
+    $("html,body").animate({scrollTop: 0 }, 500, "swing");
+  });
 
+  var winH = $(window).innerHeight();
 
+  $(window).on("scroll",function(){
+    if(winH / 2 < $(window).scrollTop()){
+      $btn.addClass("active");
+    }else{
+      $btn.removeClass("active");
+    }
+  });
 
 })();
