357bb058f5c38674b2c9c59e4cb78bd0abd9eb9f
kensuke goto <utogoken2@yahoo.co.jp>
2020-01-13 14:38:58 +0900

    一覧実装

--- js/main.js
+++ js/main.js
@@ -232,3 +232,46 @@
 
   })
 })();
+
+/**
+ * ページネーション
+ */
+(()=>{
+  $items = $(".events__inner .item");
+  // ページネーション作成
+  $pn = $(".events__pages");
+  var num = Math.ceil($items.length / 6);
+  for(var i = 1,l = num;i <= l; i++){
+    var li = "<li><a>" + i + "</a></li>";
+    $pn.append(li);
+  };
+
+  $(".events__pages li").on("click",function(){
+    var page = $(".events__pages li").index(this);
+    page += 1;
+    showItems(page);
+  })
+
+  // 何ページ目のコンテンツか
+  for(var i = 0,l = $items.length;i < l;i++){
+    var page = Math.ceil((i + 1) / 6);
+    $items.eq(i).addClass("page-" + page);
+  }
+
+  showItems(1);
+
+  function showItems(page){
+    // ページネーションのactive切り替え
+    $(".events__pages li.active").removeClass("active");
+    $(".events__pages li").eq(page - 1).addClass("active");
+        
+    $items.removeClass("active");
+    $(".events__inner").find(".page-" + page).addClass("active");
+    
+  }
+
+
+
+
+
+})();
