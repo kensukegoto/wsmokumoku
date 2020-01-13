67f616075731bebed629f71d10ab7ef5a71a9a96
kensuke goto <utogoken2@yahoo.co.jp>
2020-01-13 15:32:04 +0900

    フォームのバリデーション

--- js/main.js
+++ js/main.js
@@ -264,11 +264,67 @@
     // ページネーションのactive切り替え
     $(".events__pages li.active").removeClass("active");
     $(".events__pages li").eq(page - 1).addClass("active");
-        
+
     $items.removeClass("active");
     $(".events__inner").find(".page-" + page).addClass("active");
     
   }
+
+})();
+
+/**
+ * フォームのバリデーション
+ */
+(()=>{
+
+  $inputs = $(".contact__form input,.contact__form textarea");
+  $submit = $(".contact__form button");
+
+  $inputs.on("input",function(){
+    validate($(this));
+  });
+  $inputs.on("focus",function(){
+    validate($(this));
+  });
+
+  function validate($input){
+
+    var value = $input.val().trim();
+    var name = $input.attr("name");
+    var error = false;
+    var pattern = "";
+
+    switch(name){
+
+      case "name":
+        if(value === "") error = true;
+        break;
+      case "mail":
+        pattern = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+        if(!pattern.test(value)) error = true;
+        break;
+      case "content":
+        if(value === "") error = true;
+        break;
+      default:
+        break;
+    }
+
+    if(error){
+      $input.next().show();
+      $input.removeClass("isClear");
+    }else{
+      $input.next().hide();
+      $input.addClass("isClear");
+    }
+
+    if($inputs.length === $(".contact__form .isClear").length){
+      $submit.attr("disabled",false)
+    }else{
+      $submit.attr("disabled",true)
+    }
+  }
+
 
 
 
